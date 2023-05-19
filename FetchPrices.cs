b5af1ec4d4f74478e4e1be6dad90d58bc1f7ea80
bluehawana <bluehawana@gmail.com>
2023-05-19 22:44:05 +0200

    first commit

---
+++ FetchPrices.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace Fetch.Function
+{
+    public static class FetchPrices
+    {
+        [FunctionName("FetchPrices")]
+        public async static Task Run(
+            [TimerTrigger("0 */10 * * * *")] TimerInfo myTimer,
+            [SignalR(HubName = "coinprices")] IAsyncCollector<SignalRMessage> signalRMessages,
+            [CosmosDB(
+                databaseName: "CoinPricesDB",
+                collectionName: "CoinPrices",
+                ConnectionStringSetting = "CosmosDBConnection")] IAsyncCollector<Coin> coinsOut,
+            ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            using (var httpClient = new HttpClient())
+            {
+                var coinData = await httpClient.GetAsync("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+                log.LogInformation($"API call status: {coinData.StatusCode}");
+                var body = await coinData.Content.ReadAsStringAsync();
+                Coin[] prices = JsonSerializer.Deserialize<Coin[]>(body);
+                log.LogInformation($"Number of coins fetched: {prices.Length}");
+
+                if (prices is not null)
+                {
+                    foreach (var coin in prices)
+                    {
+                        // Log the coin symbol
+                        log.LogInformation(coin.symbol);
+
+                        // Add the coin price to Cosmos DB
+                        await coinsOut.AddAsync(coin);
+                    }
+
+                    // Send the prices to SignalR
+                    await signalRMessages.AddAsync(
+                        new SignalRMessage
+                        {
+                            Target = "updated",
+                            Arguments = new[] { JObject.FromObject(prices) }
+                        });
+                }
+            }
+        }
+    }
+
+    // Define this class based on the structure of the data returned by the CoinGecko API.
+    public class Coin
+    {
+        // Properties for the coin data (e.g., symbol, name, price, etc.)
+        public string id { get; set; }
+        public string symbol { get; set; }
+        public string image { get; set; }
+        public double current_price { get; set; }
+        public double market_cap_rank { get; set; }
+        public double price_change_percentage_24h { get; set; }
+    }
+}
